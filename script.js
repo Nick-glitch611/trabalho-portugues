bbde14298a0024a2fae3c1dcd57c3b8ed51e6252
Nicholas B <nicholas.mob1010@gmail.com>
2026-05-08 11:50:52 -0300

    Move name validation inside click event handler

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const start = document.getElementById("iniciar");
 
 start.addEventListener("click", () => {
+
+  const nome = document.getElementById("nome").value;
+  
+  if (nome === ""){
+    alert("digite seu nome")
+    return;
+  }
+    
   document.getElementById("quiz").style.display="block"
 });
-
-const nome = document.getElementById("nome").value;
-
-if (nome === ""){
-  alert("digite seu nome")
-  return;
-}
