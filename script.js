f09cccb24797405719fc92bcdd5a92ac1df4b2d2
Nicholas B <nicholas.mob1010@gmail.com>
2026-05-08 11:14:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const start = document.getElementById("iniciar");
+const start = document.getElementById("início");
 
 start.addEventListener("click", () => {
   document.getElementById("quiz").style.display="block"
