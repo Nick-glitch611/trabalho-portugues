da5788a8545cf9109b27ca1d7f7a3f1e39d0546a
Nicholas B <nicholas.mob1010@gmail.com>
2026-05-10 00:53:23 -0300

    Refactor quiz interaction and alert messages

--- script.js
+++ script.js
@@ -3,30 +3,33 @@ const start = document.getElementById("iniciar");
 start.addEventListener("click", () => {
 
   const nome = document.getElementById("nome").value;
-  
-  if (nome === ""){
-    alert("digite seu nome")
+
+  if (nome === "") {
+    alert("Digite seu nome");
     return;
   }
-    
-  document.getElementById("quiz").style.display="block"
+
+  document.getElementById("quiz").style.display = "block";
 });
 
 const resposta = document.getElementById("quiz");
 
-let atual = 1
+let atual = 1;
 
-resposta.addEventListener("click", () => {
-  document.getElementById("p" + atual).style.display="none";
-  atual++;
+resposta.addEventListener("click", (event) => {
 
-  const proxima = document.getElementById("p" + atual)
+  if (event.target.tagName === "BUTTON") {
+    
+    document.getElementById("p" + atual).style.display = "none";
 
-  if(proxima) {
-    proxima.style.display="block";
-  } else {
-    alert("fim do quiz")
-  }});
+    atual++;
 
+    const proxima = document.getElementById("p" + atual);
 
-
+    if (proxima) {
+      proxima.style.display = "block";
+    } else {
+      alert("Fim do quiz!");
+    }
+  }
+});
